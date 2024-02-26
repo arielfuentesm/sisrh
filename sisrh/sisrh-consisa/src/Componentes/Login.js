cd943d905801c5f2c7f3c50fab8a026bd5dee78e
DanielPineda19 <123615562+DanielPineda19@users.noreply.github.com>
2024-02-25 19:06:45 -0600

    Correcciones de Login
    Se aplicaron ciertas correcciones

--- sisrh/sisrh-consisa/src/Componentes/Login.js
+++ sisrh/sisrh-consisa/src/Componentes/Login.js
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import './login.css';
 
 const Login = () => {
@@ -26,39 +25,35 @@ const Login = () => {
       <div className="form-container sign-in-container">
         <form action="#">
           <h1>Iniciar Sesión</h1>
-          <span>o usa tu email</span>
-          <input type="email" placeholder="Email" />
+          <span>Utilice su Nombre de Usuario</span>
+          <input type="user" placeholder="Usuario" />
           <input type="password" placeholder="Contraseña" />
-          <a href="#">Olvidaste tu contraseña?</a>
-          <Button /*Redirigiendome hacia el incio*/
-            buttonStyle='btn--outline' onClick={() => loginWithRedirect()}  >
-            <Link to='/inicio'>Iniciar sesión</Link>
-          </Button>
+          <a href="#">¿Olvidó su contraseña?</a>
+          <button>Iniciar sesión</button>
         </form>
       </div>
       <div className="form-container sign-up-container">
         <form action="#">
           <h1>{isSignUp ? 'Registrar' : 'Crear tu cuenta'}</h1>
-          <div className="social-container">
-            {/* Social icons go here */}
-          </div>
-          <span>o usa tu email como registro</span>
-          <input type="text" placeholder="Name" />
+          <span>Ingrese los siguientes Datos</span>
+          <input type="text" placeholder="Usuario" />
+          <input type="name" placeholder="Nombre Completo" />
           <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Contraseña" />
+          <input type="confpassword" placeholder="Confirmar Contraseña" />
           <button id="lila">Registrar</button>
         </form>
       </div>
       <div className="overlay-container">
         <div className="overlay">
           <div className="overlay-panel overlay-left">
-            <h1>¡Bienvenido!</h1>
-            <p>Inicia sesión con tu cuenta</p>
+            <h1>¡Bienvenido Nuevamente a  GRUPO CONSISA!</h1>
+            <p>Inicie Sesión con su cuenta</p>
             <button className="ghost" id="signIn">Inicia sesión</button>
           </div>
           <div className="overlay-panel overlay-right">
-            <h1>Hola!!!</h1>
-            <p>Crear tu cuenta</p>
+            <h1>Bienvenido a GRUPO CONSISA</h1>
+            <p>¿No tienes cuenta?</p>
             <button className="ghost" id="signUp">Registrar</button>
           </div>
         </div>
